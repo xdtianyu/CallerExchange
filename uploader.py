2bc2f6a25dec4c48fe38f66bef51e3a59f9e1d13
tianyu <xdtianyu@gmail.com>
2016-06-01 22:01:44 +0800

    add uploader

--- uploader.py
+++ uploader.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python3
 import os
 import tarfile
+import qiniu
+
+import config
+
+access_key = config.qn_access_key
+secret_key = config.qn_secret_key
+bucket_name = config.qn_bucket_name
+
+status_file = 'cache/status.json'
 
 
 def upload(name):
-    compress(name)
+    tar_file = compress(name)
+    upload_file(tar_file)
+    upload_file(status_file)
 
 
 def compress(name):
-    tar = tarfile.open(name + ".tar.gz", "w:gz")
+    tar_name = name + ".tar.gz"
+    tar = tarfile.open(tar_name, "w:gz")
     tar.add(name, arcname=os.path.basename(name))
     tar.close()
+    return tar_name
+
+
+def upload_file(file_name):
+    q = qiniu.Auth(access_key, secret_key)
+
+    key = os.path.basename(file_name)
+
+    token = q.upload_token(bucket_name, key)
+    ret, info = qiniu.put_file(token, key, file_name)
+    if ret is not None:
+        print(file_name + ' uploaded.')
+    else:
+        print(info)
+
