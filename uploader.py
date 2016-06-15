00f468669026f0412f9868289ec6d58f5bc9308b
tianyu <xdtianyu@gmail.com>
2016-06-15 22:00:26 +0800

    add coding upload

--- uploader.py
+++ uploader.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import os
-import tarfile
 import zipfile
 
 import qiniu
+import requests
 
 import config
 
@@ -18,6 +18,8 @@ def upload(name):
     zip_file = compress(name)
     upload_file(zip_file)
     upload_file(status_file)
+    upload_file_to_coding(zip_file)
+    upload_file_to_coding(status_file)
 
 
 def compress(name):
@@ -40,3 +42,35 @@ def upload_file(file_name):
     else:
         print(info)
 
+
+def upload_file_to_coding(file_name):
+
+    class LastCommit:
+        code = -1
+        data = None
+
+        def __init__(self, s):
+            import json
+            self.__dict__ = json.loads(s)
+
+    # get last commit
+    r = requests.get(config.coding_url, cookies={'sid': config.coding_sid})
+    commit = LastCommit(r.text)
+    last_commit = None
+    try:
+        if commit.code == 0:
+            last_commit = commit.data['lastCommit']
+            print(last_commit)
+    except AttributeError:
+        pass
+
+    # make post
+    if last_commit:
+        data = {'lastCommitSha': last_commit, 'message': 'test'}
+        files = {'files': open(file_name, 'rb')}
+        cookies = {'sid': config.coding_sid}
+        r = requests.post(config.coding_url, data=data, files=files, cookies=cookies)
+        if r.text == '{"code":0}':
+            print(file_name + ' uploaded.')
+        else:
+            print(r.text)
